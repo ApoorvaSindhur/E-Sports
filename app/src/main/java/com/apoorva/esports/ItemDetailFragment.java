99a983b3934a5389c7e6555e07b2cb722c984857
ApoorvaSindhur <apoorvasindhur@gmail.com>
2017-09-10 12:29:32 +0530

    Commit 1

--- app/src/main/java/com/apoorva/esports/ItemDetailFragment.java
+++ app/src/main/java/com/apoorva/esports/ItemDetailFragment.java
@@ -15,7 +15,7 @@ import com.apoorva.esports.dummy.DummyContent;
  * A fragment representing a single Item detail screen.
  * This fragment is either contained in a {@link ItemListActivity}
  * in two-pane mode (on tablets) or a {@link ItemDetailActivity}
- * on handsets.
+ * on handsets
  */
 public class ItemDetailFragment extends Fragment {
     /**
